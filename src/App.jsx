5d4db0331d91b520dc517a99ea16481c450e5f81
BenLeland <blelan@uw.edu>
2025-04-29 15:34:06 -0700

    Added Route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import home from './components/Home';
+import user_profile from './components/User_Profile/user_profile';
+import restaurant_profile from './components/Restaurant_Profile/restaurant_profile';
 
-function App() {
+export default function App() {
 
   return (
-    <>
-
-    </>
-  )
-}
-
-export default App
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/Home' element={<home />} />
+          <Route path='/User_Profile' element={<user_profile />} />
+          <Route path='/Restaurant_Profile' element={<restaurant_profile />} />
+        </Routes>
+      </Router> 
+    </div>
+  );
+};
