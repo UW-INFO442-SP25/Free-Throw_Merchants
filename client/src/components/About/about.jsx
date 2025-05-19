48e82644501881cbc470071ba0e860c45e207c87
BenLeland <blelan@uw.edu>
2025-05-19 16:19:13 -0700

    About page basic function complete

--- client/src/components/About/about.jsx
+++ client/src/components/About/about.jsx
@@ -2,8 +2,31 @@ import './about.css';
 
 export default function About() {
     return (
-        <div>
+        <div class='about-container'>
             <h1> FoodSaver: Reducing Waste, Fighting Hunger </h1>
+
+            <h2> The Problem </h2>
+            <p>
+                According to Fourth NOAM, "In the US, around 84.3% of unused food in restaurants is thrown away while only 1.4% is donated" (Mettler, 2023). Restaurants can produce "up to 25000-75000 pounds of food waste a year" (Green Restaurant Association). Meanwhile, the UN reports that "approximately 735 million people – 9.2% of the world's population – found themselves in a state of chronic hunger in 2022" (UN SDG).
+            </p>
+
+            <h2> Our Mission </h2>
+            <p>
+                FoodSaver connects U-District businesses with excess food to individuals who need it most. We aim to reduce waste while improving access to nutrition through a simple, dignified platform.
+            </p>
+
+            <h2> Simple Process </h2>
+            <p>
+                Businesses can quickly list available food, while community members browse and claim these donations easily. Our design removes barriers and creates connections that benefit everyone.
+            </p>
+
+            <h2> Take Action Today </h2>
+            <p>
+                Whether you're a business with excess food or someone seeking nutritional support, join us in creating a community where food nourishes people, not landfills. As the UN suggests, fight food waste "at home, at work and in the community" (UN SDG).
+            </p>
+
+            <h2> More On the Topic </h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/ishA6kry8nc?si=5iMqhocMVvuQ1DgT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     );
 }
